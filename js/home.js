2a6a733c1534999493193394ae9109e010ab2114
mikef123 <miguelferu@gmail.com>
2019-07-02 18:56:21 -0500

    Update alert

--- js/home.js
+++ js/home.js
@@ -1,22 +1,20 @@
 const usuarioLogin = {
-    nombre: 'mike',
-    password: 123
+    nombre: 'admin',
+    password: 'admin'
 }
+alert('Usuario: admin \ncontraseña: admin')
 
 function iniciarSesion() {
-    debugger
     const usuario = document.getElementsByName('usrname')[0].value;
     const password = document.getElementsByName('password')[0].value;
     if (usuario !== "" && password !== "") {
         if (usuarioLogin.nombre == usuario && usuarioLogin.password == password) {
             sessionStorage.setItem("usuario", usuario);
             window.location.href = "./home.html";
-        }
-        else {
+        } else {
             swal("Upss", "El usuario o la contraseña no coinciden", "error");
         }
-    }
-    else {
+    } else {
         swal("Upss", "Falto diligenciar algún campo", "error");
     }
 }
